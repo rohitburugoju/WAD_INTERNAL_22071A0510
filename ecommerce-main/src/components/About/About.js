8933ba2b8bc2cadcea67fdcc8ba68a4629d9d306
rohitburugoju <162997672+rohitburugoju@users.noreply.github.com>
2024-06-21 17:59:18 +0530

    Update About.js

--- ecommerce-main/src/components/About/About.js
+++ ecommerce-main/src/components/About/About.js
@@ -5,8 +5,8 @@ function About() {
   return (
     <div>
         <h1>About Us</h1>
-        <h2>Welcome to Ecommerce website by ROHIT-542</h2>
-        <p>Welcome to ecommerce website where you can shop any of your use 24/7</p>
+        <h2>Welcome to Ecommerce website </h2>
+        <p>Welcome to ecommerce website where you can shop any of your use </p>
     </div>
   )
 }
